7a6d25d4170b17aaf5276f24aced556e962de544
Mominul Haque <webdesignermominulhaque@gmail.com>
2023-10-03 22:17:51 -0700

    Remosoft Preject

---
+++ asset/js/custom.js
@@ -0,0 +1,38 @@
+
+  AOS.init();
+$(document).ready(function(){
+
+  $('.back_top').click(function(){
+    $('html,body').animate(
+      {
+        scrollTop:0,
+      },500
+    )
+  })
+})
+$(document).ready(function(){
+  $('navbar-custom').click(function(){
+    var minar=$(this).scrollTop()
+    if(minar<20){
+      $('.back_top').css({
+        display:none
+      })
+
+    }
+  })
+})
+$(window).scroll(function(){
+  var minar=$(this).scrollTop();
+  if(minar >20){
+      $('.back_top').fadeIn();
+  }else{
+      $('.back_top').fadeOut();
+
+  }
+  if(minar > 20){
+      $('.navbar-custom').addClass("minar-bg")
+  }
+  else{
+      $('.navbar-custom').removeClass("minar-bg")
+  }
+})
